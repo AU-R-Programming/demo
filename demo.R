127cc09a1ca377a738dbbfa06d0b35d55996899f
robertomolinari <roberto.molinari@hotmail.it>
2026-08-25 10:44:56 -0500

    Created project and included git profile creation commands in demo.R

--- demo.R
+++ demo.R
@@ -1,5 +1,10 @@
-# Section 1
+# Commands for Git in Terminal (profile creation)
 
+git config --global user.name 'Aaron Carlson'
+git config --global user.email 'abc1234@psu.edu'
+git config --global --list
+
+# Section 1
 
 
 # Section 2
